92bad0bbc674fdcb70194812f51d87cccde9c1f1
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-06 10:53:42 +0530

    UC 2 : Gambler Makes Bet Either Win Or Loose by

--- Gambler.cs
+++ Gambler.cs
@@ -8,6 +8,8 @@ namespace GamblerProbWorkshop
     {
         public const int STACK = 100;
         public const int BET = 1;
+        public const int LOOSE_AMOUNT = 1;
+        public const int WIN_AMOUNT = 1;
         public static void gmblerOperation()
         {
         }
