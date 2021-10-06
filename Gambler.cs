ac18e5f3d450be4bdcab58214935f01c88673de0
sdmkhan123 <sdmkhan126410@gmail.com>
2021-10-06 09:58:43 +0530

    Gamber initialize

---
+++ Gambler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamblerProbWorkshop
+{
+    public class Gambler
+    {
+        public const int STACK = 100;
+        public const int BET = 1;
+        public static void gmblerOperation()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GamblerProbWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Work Shope");
+            Gambler.gmblerOperation();
+        }
+    }
+}
